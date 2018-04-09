c42dfd5a19aba67e59cd5203577ae98ee10a69b4
dangjustintime <dangjustintime@gmail.com>
2018-04-09 13:33:04 -0400

    implemented select feature, need to implement quit

--- asg4/cscomdr.sh
+++ asg4/cscomdr.sh
@@ -3,11 +3,15 @@ while true; do
   echo -e "\n-- cscomdr --"
   echo -e "$(pwd)"
   FILES=($(ls))
+  PS3="Choose an entry from the list: "
   select FILE in ${FILES[*]}; do
-    if [ "$FILE" == "q"]; then
-      echo -e "\nBye Bye"
-      break;
+    if [[ -z "$FILE" ]]; then
+      echo -e "\nfile not chosen\n"
+    elif [[ "$FILE" = "q" ]]; then
+      echo -e "\nBYE BYE\n"
+      exit 1
+    else
+      echo -e "\n$FILE"
     fi
-    
   done
 done
