acf36fc170503c0db0891d2759b99a81d13e3191
dangjustintime <dangjustintime@gmail.com>
2018-04-09 23:03:59 -0400

    working on counting type if files

--- asg4/cscomdr.sh
+++ asg4/cscomdr.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 while true; do
+  num_dir=0
+  num_exe=0
+  num_files=0
   echo -e "\n-- cscomdr --"
   echo -e "$(pwd)"
   FILES=($(ls))
-  for FILE in FILES; do
-    if [ -f $FILE ]; then
-      echo "$FILE is a file"
-    elif [ -d $FILE ]; then
-      echo "$FILE is a directory"
+  for FILE in ${FILES[*]}; do
+    if [ -d $FILE ]; then
+      ((num_dir += 1))
+    elif [ -x $FILE ]; then
+      ((num_exe += 1))
+    elif [ -f $FILE -a ! -x $FILE]; then
+      ((num_files += 1))
     fi
   done 
+  echo -e "$num_dir directories, $num_files files, $num_exe executables"
   PS3="Choose an entry from the list: "
-  select FILE in ${FILES[*]} q; do
+  select FILE in ${FILES[*]}; do
     if [[ -z "$FILE" ]]; then
       echo -e "\nBYE BYE\n"
       exit 1
