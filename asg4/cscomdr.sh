815863ece8050d6b8a8abecbef4b30153560c3b2
dangjustintime <dangjustintime@gmail.com>
2018-04-11 20:02:35 -0400

    fixed slicing

--- asg4/cscomdr.sh
+++ asg4/cscomdr.sh
@@ -9,19 +9,31 @@ while true; do
   files=($(ls))
   for file in ${files[*]}; do
     if [ -d $file ]; then
-      files[index]="DIR:${files[index]}"
+      files[index]=DIR:${files[index]}
       ((num_dir++))
     elif [ -f $file -a ! -x $file ]; then
       ((num_files++))
     elif [ -x $file ]; then
-      files[index]="EXE:${files[index]}"
+      files[index]=EXE:${files[index]}
       ((num_exe++))
     fi
     ((index++))
   done 
   echo -e "$num_dir directories, $num_files files, $num_exe executables"
+  PS1="111111111"
+  PS2="222222222"
   PS3="Choose an entry from the list: "
   select file in ${files[*]}; do
+    case $file in
+      "EXE:"*)
+	file=${file:4}
+        ;;
+      "DIR:"*)
+	file=${file:4}
+        ;;
+      *)
+	;;
+    esac
     if [[ -z "$file" ]]; then
       echo -e "\nBYE BYE\n"
       exit 1
@@ -29,9 +41,9 @@ while true; do
       cat $file
       echo -e "\n"
     elif [ -d $file ]; then
-      echo "${file:4}"
+      echo $file
     elif [ -x $file ]; then
-      echo "${file:4}"
+      echo $file
     else
       echo -e "\n$file"
     fi
