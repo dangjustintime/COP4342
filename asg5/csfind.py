2c86fdb25242b39c34499d050bcd49feb85b7bca
dangjustintime <dangjustintime@gmail.com>
2018-04-30 12:48:55 -0400

    added flags

--- asg5/csfind.py
+++ asg5/csfind.py
@@ -3,6 +3,13 @@ import os, sys
 from stat import *
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
+# name flag
+parser.add_argument('-name')
+# grep flag
+parser.add_argument('-grep')
+args = parser.parse_args()
 
-
+if args.name:
+	print "NAME"
+elif args.grep:
+	print "GREP"
