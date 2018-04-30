6ae834cadde20f69ef777730be3eedd95c88db36
dangjustintime <dangjustintime@gmail.com>
2018-04-30 13:31:58 -0400

    put script into main function

--- asg5/csfind.py
+++ asg5/csfind.py
@@ -2,14 +2,19 @@ import argparse
 import os, sys
 from stat import *
 
-parser = argparse.ArgumentParser()
-# name flag
-parser.add_argument('-name')
-# grep flag
-parser.add_argument('-grep')
-args = parser.parse_args()
+def main():
+	parser = argparse.ArgumentParser()
+	# name flag
+	parser.add_argument('-name')
+	# grep flag
+	parser.add_argument('-grep')
+	args = parser.parse_args()
 
-if args.name:
-	print "NAME"
-elif args.grep:
-	print "GREP"
+	if args.name:
+		print "NAME"
+		print args.name
+	if args.grep:
+		print "GREP"
+		print args.grep
+
+if __name__ == '__main__': main()
